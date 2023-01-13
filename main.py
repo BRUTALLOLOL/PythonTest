2bf0a52f7932f054aa71ab9edaa781196ebe5b6d
Xiesta <artem08v@gmail.com>
2023-01-13 20:03:15 +0500

    decorator

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+
+
+def repeat_three_times(func):
+    def wrapper(*args, **kwargs):
+        for i in range(3):
+            print(i+1)
+            func(*args, **kwargs)
+    return wrapper
+
+@repeat_three_times
 def sum_of_squares(*args, **kwargs):
     result = 0
 
@@ -9,7 +19,8 @@ def sum_of_squares(*args, **kwargs):
     for i in list(kwargs.items()):
         result += int(i[1]) ** 2
 
-    return result
+    print(result)
 
 
-print(sum_of_squares(1, 2, 3, a=1, b=3))
+sum_of_squares(1, 2, 3,  a=1, b=3)
+
