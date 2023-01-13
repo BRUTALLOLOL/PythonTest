f792a0fe8f05025b1836fe42ff4802120479dc66
Xiesta <artem08v@gmail.com>
2023-01-13 19:44:16 +0500

    args and kwargs

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello')
+def sum_of_squares(*args, **kwargs):
+    result = 0
+
+    # Позиционные аргументы
+    for i in args:
+        result += int(i) ** 2
+
+    # Именованные аргументы
+    for i in list(kwargs.items()):
+        result += int(i[1]) ** 2
+
+    return result
+
+
+print(sum_of_squares(1, 2, 3, a=1, b=3))
