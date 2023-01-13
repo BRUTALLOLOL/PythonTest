2fa0b1ca77dead5642e0cee75d48241c921962fe
Xiesta <artem08v@gmail.com>
2023-01-13 21:46:22 +0500

    OOP

--- main.py
+++ main.py
@@ -1,35 +1,68 @@
-def repeat_three_times(func):
-    def wrapper(*args, **kwargs):
-        for i in range(3):
-            print(i + 1)
-            func(*args, **kwargs)
+class Item:
+    def __init__(self):
+        self.to_do_list = dict()
 
-    return wrapper
+    def add_item(self, name):
+        time_frame = int(input('Insert time frame: '))
+        priority = int(input('Insert priority from 1 to 10: '))
+        self.to_do_list.update({name: [f'Time: {time_frame}', f'Priority: {priority}']})
+        print('\n' * 2)
+        print('-YOUR ITEMS-')
+        print(list(self.to_do_list.keys()))
+        print('\n' * 2)
+
+class ItemList(Item):
+    def get_all_items(self):
+        print('-YOUR ITEMS-')
+        print(list(self.to_do_list.keys()))
+        print('\n' * 2)
+
+    def get_item_by_name(self, name):
+        print('-ITEM INFO-')
+        print(f'Name: {name}')
+        print(self.to_do_list.get(name)[0])
+        print(self.to_do_list.get(name)[1])
+        print('\n'*2)
+
+    def get_items_by_time(self, time):
+        print(f'-ITEMS BY TIME {time}-')
+        result = []
+        for i in list(self.to_do_list.keys()):
+            if self.to_do_list.get(i)[0] == f'Time: {time}':
+                result.append(i)
+        print(result)
+        print('\n' * 2)
 
 
-@repeat_three_times
-def sum_of_squares(*args, **kwargs):
-    result = 0
+    def get_priority_items(self):
+        print(f'-PRIORITY ITEMS-')
+        result = []
+        for i in list(self.to_do_list.keys()):
+            if int(self.to_do_list.get(i)[1][10:]) > 5:
+                result.append(i)
+        print(result)
+        print('\n' * 2)
 
-    # Позиционные аргументы
-    for i in args:
-        try:
-            result += i ** 2
-        except:
-            print('exception')
-            result += int(i) ** 2
+    def delete_item(self, name):
+        self.to_do_list.pop(name)
+        print(f'-YOU DELETED {name} TASK-')
+        print('\n' * 2)
 
-    # Именованные аргументы
-    for i in list(kwargs.items()):
-        try:
-            result += i[1] ** 2
-        except:
-            print('exception')
-            result += int(i[1]) ** 2
+    def change_time(self, name, time):
+        self.to_do_list[name][0] = f'Time: {time}'
 
-    print(result)
+    def change_priority(self, name, priority):
+        self.to_do_list[name][1] = f'Priority: {priority}'
 
-
-sum_of_squares(1, "2", 3, a=1, b=3)
-
-sum_of_squares(1, "two", 3, a=1, b=3)
+a = ItemList()
+a.add_item("Example")
+a.add_item("Another")
+a.get_all_items()
+a.get_item_by_name("Another")
+a.get_items_by_time("1")
+a.get_priority_items()
+a.change_priority("Another", 10)
+a.change_time("Another", 2)
+a.get_item_by_name("Another")
+a.delete_item("Another")
+a.get_all_items()
