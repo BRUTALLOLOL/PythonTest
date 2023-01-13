44812c2248b72f0ced9b3903a4c01acdfb45f86f
Xiesta <artem08v@gmail.com>
2023-01-13 20:17:39 +0500

    errors

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
-
 def repeat_three_times(func):
     def wrapper(*args, **kwargs):
         for i in range(3):
-            print(i+1)
+            print(i + 1)
             func(*args, **kwargs)
+
     return wrapper
+
 
 @repeat_three_times
 def sum_of_squares(*args, **kwargs):
@@ -13,14 +13,23 @@ def sum_of_squares(*args, **kwargs):
 
     # Позиционные аргументы
     for i in args:
-        result += int(i) ** 2
+        try:
+            result += i ** 2
+        except:
+            print('exception')
+            result += int(i) ** 2
 
     # Именованные аргументы
     for i in list(kwargs.items()):
-        result += int(i[1]) ** 2
+        try:
+            result += i[1] ** 2
+        except:
+            print('exception')
+            result += int(i[1]) ** 2
 
     print(result)
 
 
-sum_of_squares(1, 2, 3,  a=1, b=3)
+sum_of_squares(1, "2", 3, a=1, b=3)
 
+sum_of_squares(1, "two", 3, a=1, b=3)
